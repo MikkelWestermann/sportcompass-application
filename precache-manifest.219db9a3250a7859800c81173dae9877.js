3f804fcaae0036e4108066e8c09f2d0fed897e86
Mikkel Westermann <mikkelwestermann@Mikkels-iMac.local>
2019-01-18 21:11:13 +0100

    Updates

---
+++ precache-manifest.219db9a3250a7859800c81173dae9877.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "088badf6cfc3d6a9cf34",
+    "url": "/sportcompass-application/static/css/main.1c13ba25.chunk.css"
+  },
+  {
+    "revision": "088badf6cfc3d6a9cf34",
+    "url": "/sportcompass-application/static/js/main.088badf6.chunk.js"
+  },
+  {
+    "revision": "4188a5ae3e830e43f16c",
+    "url": "/sportcompass-application/static/css/1.51897aff.chunk.css"
+  },
+  {
+    "revision": "4188a5ae3e830e43f16c",
+    "url": "/sportcompass-application/static/js/1.4188a5ae.chunk.js"
+  },
+  {
+    "revision": "be37b071a98c795ba16e",
+    "url": "/sportcompass-application/static/js/runtime~main.be37b071.js"
+  },
+  {
+    "revision": "6251a8dc4afa898a1b56f4ff4080bf82",
+    "url": "/sportcompass-application/static/media/Robots_illustration.6251a8dc.jpg"
+  },
+  {
+    "revision": "3b1178fce484d49dfa227c7b8281bed6",
+    "url": "/sportcompass-application/index.html"
+  }
+];
